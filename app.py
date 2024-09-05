f5d94e10fdd89abe7b1f910f7d4677136a5c1d0e
Russell Salazar <83458251+Russell0308@users.noreply.github.com>
2024-09-04 23:11:01 -0400

    bug fix

--- app.py
+++ app.py
@@ -1,24 +1,24 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template
 from nba_api.stats.library import data
 import pandas as pd
 
+
 app = Flask(__name__)
+
 
 @app.route("/")
 def root():
-        return render_template('templates/index.html')
+        return render_template('index.html')
 
 
 @app.route("/player_finder")
-def player_finder():
+def player_lookup():
     player_search = data.players
 
     player_df = pd.DataFrame(player_search)
     
-    return player_df
 
-
+    return 'hello world!'
 
     
 
