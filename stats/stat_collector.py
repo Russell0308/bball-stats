261e14475fda87d2c5cc71d4d5d1f7adfda7a890
Russell Salazar <83458251+Russell0308@users.noreply.github.com>
2024-10-17 23:49:07 -0400

    update

--- stats/stat_collector.py
+++ stats/stat_collector.py
@@ -6,12 +6,18 @@ from nba_api.stats.static import teams
 
 import pandas as pd
 
+import os
+
 #  #
 def get_basic_player_df():
     '''
     Gets (id, last name, first name, full name, activity)
     '''
-    basic_player_df = pd.DataFrame(data.players)
-    basic_player_df.columns = ['ids', 'last_name', 'first_name', 'full_name', 'is_active']
-    basic_player_df.to_csv('./CSVs/basic_player_data.csv', index=False)
+
+    if os.path.isfile('./CSVs/basic_player_data.csv') == True:
+        pass
+    else:
+        basic_player_df = pd.DataFrame(data.players)   
+        basic_player_df.columns = ['ids', 'last_name', 'first_name', 'full_name', 'is_active']
+        basic_player_df.to_csv('./CSVs/basic_player_data.csv', index=False)
 
