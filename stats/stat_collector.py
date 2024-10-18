9013da9ab2d655c78273a3182472818634cce347
Russell Salazar <83458251+Russell0308@users.noreply.github.com>
2024-10-17 23:57:13 -0400

    clean up

--- stats/stat_collector.py
+++ stats/stat_collector.py
@@ -8,16 +8,25 @@ import pandas as pd
 
 import os
 
-#  #
+
+def create_basic_player_data_csv():
+    # Does file exist?
+    if os.path.isfile('./CSVs/basic_player_data.csv') == True:
+        pass
+    else:                     # Create file
+        basic_player_df = pd.DataFrame(data.players)   
+        basic_player_df.columns = ['ids', 'last_name', 'first_name', 'full_name', 'is_active']
+        basic_player_df.to_csv('./CSVs/basic_player_data.csv', index=False)
+
+
 def get_basic_player_df():
     '''
     Gets (id, last name, first name, full name, activity)
     '''
+    create_basic_player_data_csv()
 
-    if os.path.isfile('./CSVs/basic_player_data.csv') == True:
-        pass
-    else:
-        basic_player_df = pd.DataFrame(data.players)   
-        basic_player_df.columns = ['ids', 'last_name', 'first_name', 'full_name', 'is_active']
-        basic_player_df.to_csv('./CSVs/basic_player_data.csv', index=False)
+    df = pd.DataFrame('./CSVs/basic_player_data.csv')
+
+    return df
+
 
