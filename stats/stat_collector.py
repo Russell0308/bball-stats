4030d8e3a04a6bb3bfa88b7af4df32d74376e99a
Russell Salazar <83458251+Russell0308@users.noreply.github.com>
2024-10-20 15:18:59 -0400

    Team abbreviations added, player csvs working

--- stats/stat_collector.py
+++ stats/stat_collector.py
@@ -30,3 +30,90 @@ def get_basic_player_df():
     return df
 
 
+#def update_basic_player_df():
+#    location = './stats/CSVs/basic_player_data.csv'
+#    
+#    basic_player_df = pd.DataFrame(data.players)
+#    basic_player_df.columns = ['ids', 'last_name', 'first_name', 'full_name', 'is_active']
+#
+#    basic_player_df.to_csv(location, index=False)
+
+
+def create_basic_teams_csv():
+    if os.path.isfile('./stats/CSVs/basic_teams.csv') == True:
+        pass
+    else:
+        basic_teams_df = pd.DataFrame(teams.get_teams())    
+        basic_teams_df.to_csv('./stats/CSVs/basic_teams.csv', index=False)
+
+
+def get_teams_df():
+    create_basic_teams_csv()
+    
+    df = pd.read_csv('./stats/CSVs/basic_teams.csv')
+    
+    return df
+
+
+def create_player_profile_csv(player_id):
+    if os.path.isfile(f'./stats/CSVs/playerprofiles/{player_id}.csv') == True:
+        pass
+    else:
+        try:
+            player_profile_df = pd.DataFrame(playerprofilev2.PlayerProfileV2(player_id=player_id).get_data_frames()[0])
+            player_profile_df.to_csv(f'./stats/CSVs/playerprofiles/{player_id}.csv')
+        except:
+            print(player_profilev2.PlayerProfile(player_id=player_id))
+            return Exception
+
+    
+def get_player_profile_df(player_id):
+    create_player_profile_csv(player_id)
+
+    df = pd.read_csv(f'./stats/CSVs/playerprofiles/{player_id}.csv')
+
+    return df
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- stats/stat_server.py
+++ stats/stat_server.py
@@ -4,10 +4,6 @@ from flask import url_for
 
 from stats import stat_collector as statc
 
-from nba_api.stats.library import data
-from nba_api.stats.endpoints import playerprofilev2, commonteamroster
-from nba_api.stats.static import teams
-
 from thefuzz import process
 
 import pandas as pd
@@ -21,59 +17,39 @@ players_df.drop('first_name', inplace=True, axis=1)
 players_df = players_df[players_df['is_active'] == True]
 players_df.drop('is_active', inplace=True, axis=1)
 
-def get_player_name_list():
-    return players_df['full_name']
-
-
-def get_teams_data():
-    teams_df = pd.DataFrame(teams.get_teams())
-    return teams_df
-
-
-def get_player_profile(player_id):
-    playerprofile = playerprofilev2.PlayerProfileV2(player_id=player_id)
-    return playerprofile
-
 
 def get_team_roster(team_id):
     team_roster = commonteamroster.CommonTeamRoster(team_id=team_id)
     return team_roster
 
-def get_search_result_dash(user_query):
-    full_names_list = get_player_name_list()
-    names_df = pd.DataFrame(full_names_list, index=None)
-    names_df.columns = ['Full_Name']
 
-    user_search_result = process.extract(user_query, names_df['Full_Name'], limit=5)
+def get_search_result_dash(user_query):
+    user_search_result = process.extract(user_query, players_df['full_name'], limit=5)
 
     result_df = pd.DataFrame()
-    
+
     user_names_result = []
     for i in user_search_result:
         user_names_result.append(i[0])
-    result_df['Full_Name'] = user_names_result
-    print(result_df.head)
+    result_df['full_name'] = user_names_result
 
     player_links_list = []
     player_team_abbrev_list = []
     player_number_list = []
 
-    for i in result_df['Full_Name']:
-        result_df['Link_Names'] = result_df['Full_Name'].apply(lambda x: re.sub(r'\s+', '_', x))
+    for i in result_df['full_name']:
+        result_df['link_names'] = result_df['full_name'].apply(lambda x: re.sub(r'\s+', '_', x))
+        player_name = i
 
-      #  row = search_results.loc[search_results['Full_Name'] == player_name]
-        #result_df = pd.concat((result_df, row), axis=0, ignore_index=True)
+        player_row = players_df[players_df['full_name'] == player_name]
+        player_id = player_row['ids'].iloc[0]
 
-        #player_row = players_df[players_df['Full_Name'] == player_name]
-        #player_id = player_row['Ids'].iloc[0]
-
-        #player_profile = get_player_profile(player_id).get_data_frames()
-        #player_team_abbrev = list(player_profile[0]['TEAM_ABBREVIATION'])[-1]
-        #if player_team_abbrev == 'TOT':
-        #    player_team_abbrev = list(player_profile[0]['TEAM_ABBREVIATION'])[-2]
-        #player_team_abbrev_list.append(player_team_abbrev)
+        player_profile = statc.get_player_profile_df(player_id)
+        player_team_abbrev = list(player_profile['TEAM_ABBREVIATION'])[-1]
+        if player_team_abbrev == 'TOT':
+            player_team_abbrev = list(player_profile['TEAM_ABBREVIATION'])[-2]
+        player_team_abbrev_list.append(player_team_abbrev)
        
-
         #team_df = get_teams_data()
         #team_id = team_df[team_df['abbreviation'] == player_team_abbrev]['id']
 
@@ -81,18 +57,18 @@ def get_search_result_dash(user_query):
 
         #player_data = team_roster[team_roster['PLAYER'] == player_name]
         #player_number = player_data['NUM']
-        #print(player_number)
         #player_number = int(player_number.iloc[0])
         #player_number_list.append(player_number)
-    for x, y in zip(result_df['Full_Name'], result_df['Link_Names']):
+
+    for x, y in zip(result_df['full_name'], result_df['link_names']):
         player_links = f'''<a href=" { url_for('player_fullscreen', player_name=y) } ">{x}</a>'''
         player_links_list.append(player_links)
 
-    result_df['Player_Link'] = player_links_list
-    #result_df['Abbreviations'] = player_team_abbrev_list
+    result_df['player_link'] = player_links_list
+    result_df['Abbreviations'] = player_team_abbrev_list
     #result_df['Player_Number'] = player_number_list
-    result_df.drop('Full_Name', axis=1, inplace=True)
-    result_df.drop('Link_Names', axis=1, inplace=True)
+    result_df.drop('full_name', axis=1, inplace=True)
+    result_df.drop('link_names', axis=1, inplace=True)
         
     return result_df
 
