8d76b1e5c7cb2c207afc913c3fb877dfd480f663
Russell Salazar <83458251+Russell0308@users.noreply.github.com>
2024-10-23 06:24:58 -0400

    small bug fixes

--- stats/stat_server.py
+++ stats/stat_server.py
@@ -19,11 +19,11 @@ players_df.drop('is_active', inplace=True, axis=1)
 
 
 def get_search_result_dash(user_query):
-    user_search_result = process.extract(user_query, players_df['full_name'], limit=10)
+    user_search_result = process.extract(user_query, players_df['full_name'], limit=10) #Fuzzy find top ten active players names using user search 'term'/'name'. 
 
     result_df = pd.DataFrame()
 
-    user_names_result = []
+    user_names_result = [] # Cut out all the names we don't care about decreases load time immensely
     for i in user_search_result:
         user_names_result.append(i[0])
     result_df['full_name'] = user_names_result
@@ -33,17 +33,17 @@ def get_search_result_dash(user_query):
     player_number_list = []
 
     for i in result_df['full_name']:
-        result_df['link_names'] = result_df['full_name'].apply(lambda x: re.sub(r'\s+', '_', x))
+        result_df['link_names'] = result_df['full_name'].apply(lambda x: re.sub(r'\s+', '_', x)) # Add underscores to players names for links to players page
         player_name = i
 
         player_row = players_df[players_df['full_name'] == player_name]
-        player_id = player_row['ids'].iloc[0]
+        player_id = player_row['ids'].iloc[0] # Get player id
 
-        players_team_id = statc.get_players_team_id(player_id)
+        players_team_id = statc.get_players_team_id(player_id) # Get players team id
 
         teams = pd.DataFrame(statc.get_teams_df())
         teams = teams[teams['id'] == players_team_id]
-        if teams['abbreviation'].empty == True:
+        if teams['abbreviation'].empty == True:  # If players don't have a valid team id they are removed from result_df
             result_df = result_df.loc[result_df['full_name'] != player_name]
             continue
         player_team_abbrev_list.append(teams['abbreviation'].iloc[0])
@@ -53,16 +53,20 @@ def get_search_result_dash(user_query):
         player_data = team_roster[team_roster['PLAYER'] == player_name]
 
         player_number = player_data['NUM']
-        player_number = int(player_number.iloc[0])
+        if player_number.empty == True:
+            player_number = 'N/A'
+        else:
+            player_number = int(player_number.iloc[0])
         player_number_list.append(player_number)
 
     for x, y in zip(result_df['full_name'], result_df['link_names']):
         player_links = f'''<a href=" { url_for('player_fullscreen', player_name=y) } ">{x}</a>'''
         player_links_list.append(player_links)
 
+
+    result_df['Player_Number'] = player_number_list
     result_df['player_link'] = player_links_list
     result_df['Abbreviations'] = player_team_abbrev_list
-    result_df['Player_Number'] = player_number_list
     result_df.drop('full_name', axis=1, inplace=True)
     result_df.drop('link_names', axis=1, inplace=True)
 
